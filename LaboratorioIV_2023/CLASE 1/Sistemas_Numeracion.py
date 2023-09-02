7a1ac2f5fd009761453016f659c3a2cc20c08639
Roberto Edgardo Perez Bertoli <91570445+robertoperez2793@users.noreply.github.com>
2023-09-02 18:32:04 -0300

    Add files via upload

---
+++ LaboratorioIV_2023/CLASE 1/Sistemas_Numeracion.py
@@ -0,0 +1,36 @@
+#Profundizando en los sistemas de numeracion
+
+# Sistema decimal
+a = 10
+
+#Sistema binario
+a = 0b1010
+
+#Sistema octal
+a = 0o12
+
+print(f'a: {a}')
+
+#Sistema hexadecimal
+a = 0xA
+print(f'a hexadecimal: {a}')
+
+a = int('23', 10)
+print(f'a decimal: {a}')
+
+#Base binario
+a = int('1010', 2)
+print(f'a binario: {a}')
+
+# base octal
+a = int(f'12', 8)
+print(f' a = base octal: {a}')
+
+#Base hexadecimal
+a = int('17', 16)
+print(f'a= base hexadecimal: {a} ')
+
+#Base 5
+
+a = int('34', 5)
+print(f'a = base 5: {a}')
---
+++ LaboratorioIV_2023/CLASE 1/Tipo_Float.py
@@ -0,0 +1,12 @@
+#Profundizamos en el tipo float
+a = 3.0
+print(f'a: {a:.2f}')
+
+# Constructor de tipo float -> puede recibir int y string
+a = float(10)
+a = float('10')
+print(f'a: {a:.2f}')
+
+# notacion exponencial (valores positivos o negativos)
+a = 3e5
+print(f'9a: {a}')
