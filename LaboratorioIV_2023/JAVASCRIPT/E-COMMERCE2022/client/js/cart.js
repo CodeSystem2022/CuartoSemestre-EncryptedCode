d14941c1335dcfadb38f0e51fbebc713daa6059b
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-09-19 13:22:00 -0300

    Update cart.js

--- LaboratorioIV_2023/JAVASCRIPT/E-COMMERCE2022/client/js/cart.js
+++ LaboratorioIV_2023/JAVASCRIPT/E-COMMERCE2022/client/js/cart.js
@@ -1,7 +1,10 @@
+const { error } = require("console");
+
 const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
-const cartBtn = document.getElementById("cart.btn");
+const cartBtn = document.getElementById("cart-btn");
+const cartCounter = document.getElementById("cart-counter")
 
 const dispayCart = () =>{
     modalContainer.innerHTML = "";
@@ -13,14 +16,14 @@ const dispayCart = () =>{
     const modalClose = document.createElement("div");
     modalClose.innerText = "❌"
     modalClose.className = "modal-close";
-    modalHeader.append(modalClose)
+    modalHeader.append(modalClose);
 
     modalClose.addEventListener("click", ()=>{
         modalContainer.style.display = "none";
         modalOverlay.style.display = "none";
     })
 
-    constmodalTitle = document.createElement("div");
+    const modalTitle = document.createElement("div");
     modalTitle.innerText = "Cart";
     modalTitle.className = "modal-title";
     modalHeader.append(modalTitle);
@@ -28,6 +31,7 @@ const dispayCart = () =>{
     modalContainer.append(modalHeader);
     
     //modal Body
+    if(cart.length > 0){}
     cart.forEach(product => {
         const modalBody = document.createElement("div");
         modalBody.className = "modal-body";
@@ -47,12 +51,13 @@ const dispayCart = () =>{
             </div>
             `;
         modalContainer.append(modalBody);
+    
 
         const decrese = modalBody.querySelector(".quantity-btn-decrese");
         decrese.addEventListener("click", ()=>{
             if(product.quanty !== 1){
             product.quanty--;
-            dispayCart();
+            displayCart();
 
         };
     
@@ -71,23 +76,99 @@ const dispayCart = () =>{
     deleteProduct.addEventListener("click", ()=>{
         deleteCartProduct(product.id);
     })
+});
 
     //modal fotter
-const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
+    const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
 
     const modalFooter = document.createElement("div");
     modalFooter.className = "modal-footer";
     modalFooter.innerHTML = `
     <div class="total-price">Total: ${total}</div>
-    `
+    <button class="btn-primary" id="checkout-btn"> go to checkout</button>
+    <div id="button-checkout"></div>
+
+    `;
     modalContainer.append(modalFooter);
-});
+    //app
+    const mercadopago = new MercadoPago("public_sky",  { 
+        locale: "es-AR", //the most common are: 'pt-BR' and 'en-US'
+    });
+
+    constcheckoutButton = modalFooter.querySelector("#checkout-btn");
+
+    checkoutButton.addEventListener("click", function(){
+        checkoutButton.remove();
+
+        const orderData = {
+            quantity: 1,
+            description: "compra de ecommerce",
+            prive: total,
+        },
+        fatch ("http://localhost:8080/create_preference",{
+            method: "POST",
+            Headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(orderData),
+        })
+        .then(function (response){
+            return response.JSON();
+        })
+        .then(function(preference){
+            createCheckoutButton(preference.id);
+        })
+        .catch(function(){
+            alert("Unexpected error");
+        })
+    });
+    function createCheckoutButton(preferencia) {
+        //Initialize the checkout
+        const bricksBuilder = mercadopago.bricks();
+
+        const renderComponent = async (bricksBuilder) => {
+            //if (windows.checkoutButton) checkout.unmount();
+
+            await bricksBuilder.create{
+                "wallet",
+                "button-checkout", //class/id where the payment button will be displayed
+                {
+                    initialization: {
+                        preferenceId: preferenceId,    
+                    },
+                    callbacks: {
+                        onError: (error) => console.error(error),
+                        onReady: () => {},
+                    },
+                }
+            }
+        };
+        window.checkoutButton = renderComponent(bricksBuilder); 
+    } 
+ }else{
+    const modalText = document.createElement("h2");
+    modalClose.className = "modal-body";
+    modalText.innerText = "your cart is empty";
+    modalContainer.append(modalText);
+
+}
+};
 
 cartBtn.addEventListener("click", dispayCart);
-};
 const deleteCartProduct =(id)=>{
     const foundId = cart.findIndex((element)=> element.id === id)
     console.log(foundId);
     cart.splice(foundId, 1);
-    displayCart();
+    dispayCart();
+}
+
+const dispayCartCounter =()=>{
+     const cartLength = cart.reduce((acc, el) => acc + el.quanty, 0);
+     if(cartLength > 0){
+        cartCounter.style.display = "block";
+        cartCounter.innerText = cartLength; 
+    }
+   
+    
+
 }
