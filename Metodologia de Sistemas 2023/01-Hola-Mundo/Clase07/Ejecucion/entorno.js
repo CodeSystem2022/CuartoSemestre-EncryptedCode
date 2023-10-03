2069b398743b505afeceff033bb136c91a5d01dc
Andrea Llavel <112596102+AndreaLlavel@users.noreply.github.com>
2023-10-03 20:50:35 +0200

    Delete Metodologia de Sistemas 2023/01-Hola-Mundo/Clase07/Ejecucion directory
    arreglando ubicacion

--- Metodologia de Sistemas 2023/01-Hola-Mundo/Clase07/Ejecucion/entorno.js
+++
@@ -1,7 +0,0 @@
-let nombre = process.env.NOMBRE || 'Sin nombre';
-let web = process.env.MI_WEB || 'nO TENGO WEB';
-
-console.log('Hola '+nombre);
-console.log('Mi web es: '+web);
-
-console.log('Ultima Ejecucion');
