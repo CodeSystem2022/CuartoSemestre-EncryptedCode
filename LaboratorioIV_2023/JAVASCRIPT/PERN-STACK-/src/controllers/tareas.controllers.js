073a814ea2b9fccff798e91b3b29912b4dcb55c4
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-11-04 23:51:33 -0300

    Update tareas.controllers.js

--- LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/controllers/tareas.controllers.js
+++ LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/controllers/tareas.controllers.js
@@ -1,65 +1,59 @@
-import{pool} from "../db.js";
+import { pool } from '../db.js';
 
-export const listarTareas = async (req, res) => {
-    console.log(req.usuarioId);
-    const resultado = await pool.query('SELECT * FROM tareas');
+export const listarTareas = async (req, res, next) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE usuario_id = $1', [req.usuarioId]);
     return res.json(resultado.rows);
-    }
-    ;
-
-export const listarTare =  async (req, res) => {
-    const resultado = await pool.query('SELECT * FROM tareas WHERE id=$1', [req.params.id])
-    if(resultado.rowCount === 0) {
+}
+export const listarTarea = async (req, res) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0) {
         return res.status(404).json({
-            message: "la tarea no existe",
+            message: 'La tarea no existe'
         });
     }
-    return res.json(resultado);
-};
+    return res.json(resultado.rows[0]);
+}
 
-export const crearTarea = async(req, res, next) => {
-    const { titulo, descripcion} = req.body;
-    
+
+export const crearTarea = async (req, res, next) => {
+    const { titulo, descripcion } = req.body;
+
 
     try {
-        throw new Error('algo salio mal');
-        const result = await pool.query('INSERT INTO tares(titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
+
+        const result = await pool.query('INSERT INTO tareas ( titulo, descripcion, usuario_id ) VALUES ($1, $2, $3) RETURNING *', [titulo, descripcion, req.usuarioId]);
         res.json(result.rows[0]);
+        console.log(result.rows[0]);
     } catch (error) {
         if (error.code === '23505') {
             return res.status(409).json({
-                message: 'ya existe una tarea con ese titulo',
+                message: 'Ya existe una tarea con ese titulo'
             });
-            
         }
         console.log(error);
         next(error);
- } };
-export const actualizarTarea = async (req, res) => {
-    const { titulo, descripcion} = req.body;
-    const id = req.params.id;
-    const result = await pool.query("UPDATE tareas SET titulo = $1, descrpcion = $2 WHERE id = $3 RETURNING*" [titulo, descripcion, id])
+    }
+};
 
-    if(result.rowCount === 0){
+export const actualizarTarea = async (req, res) => {
+    const { titulo, descripcion } = req.body;
+    const id = req.params.id;
+    const result = await pool.query('UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3 RETURNING *', [titulo, descripcion, id]);
+
+    if (result.rowCount === 0) {
         return res.status(404).json({
             message: 'No existe una tarea con ese id'
         });
-
     }
     return res.json(result.rows[0]);
 }
+    export const eliminarTarea = async (req, res) => {
+        const resultado = await pool.query('DELETE FROM tareas WHERE id = $1', [req.params.id]);
 
-export const eliminarTarea = async (req, res) => {
-    const resultado = await pool.query('DELETE FROM tareas WHERE id= $1', [req.params.id])
-    console.log(resultado);
-
-    if (resultado.rowCount === 0) {
-        return res.status(404).json({
-            message: 'No existe la tarea con ese id'
-        })
+        if (resultado.rowCount === 0) {
+            return res.status(404).json({
+                message: 'No existe una tarea con ese id'
+            });
+        }
+        return res.sendStatus(204);
     }
-    return res.sendStatus(204);
-}
-    }
-    return res.sendStatus(204);
-}
