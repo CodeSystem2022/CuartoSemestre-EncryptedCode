b27a1d52cdf12374104180ecb5d441d2f4e56f9b
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-11-04 23:59:04 -0300

    Update app.js

--- LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/app.js
+++ LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/app.js
@@ -2,25 +2,37 @@ import express from "express";
 import morgan from "morgan";
 import tareasRoutes from "./router/tareas.routes.js"
 import authRoutes from "./router/auth.routes.js"
-import cookiePaser from "cookie-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { pool } from "./db.js";
+import { ORIGIN } from "./config.js";
 
 const app = express();
 //Middlewares
 app.use(morgan("dev"));
-app.use(cookiePaser());
+app.use(cors(
+  {
+    origin: ORIGIN,
+    credentials: true
+  }
+));
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.json({ message: "Bienvenidos a mi proyecto"}));
-app.use(tareasRoutes)
-app.use(authRoutes);
-
-//manejando errores
-app.use((req, res, next) => {
-    res.status(500).json({
-        status: "error",
-        message: err.messagge
-    });
+app.get("/", (req, res) => res.json({ message: "Bienvenidos a mi proyecto" }));
+app.get("/api/ping", async(req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows[0]);
+});
+app.use('/api',tareasRoutes);
+app.use('/api',authRoutes)
+//Manejando errores
+app.use((err, req ,res, next) => {
+  res.status(500).json({
+    status: "error",
+    message: err.message
+  });
 });
 
 export default app;
