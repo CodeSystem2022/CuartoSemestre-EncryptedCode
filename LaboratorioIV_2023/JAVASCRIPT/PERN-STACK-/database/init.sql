73998b220d05c8baeccbf32bf51175b3616e08dc
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-11-04 23:49:00 -0300

    Update init.sql

--- LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/database/init.sql
+++ LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/database/init.sql
@@ -1,12 +1,22 @@
-CREATE TABLE tareas(
+CREATE TABLE tareas (
     id SERIAL PRIMARY KEY,
-    titulo VARCHAR(255) UNIQUE not NULL,
+    titulo VARCHAR(255) UNIQUE NOT NULL,
     descripcion TEXT,
 );
-CREATE TABLE usuarios(
+
+ALTER TABLE tareas ADD COLUMN usuario_id INTEGER REFERENCES usuarios(id);
+
+--remove unique from titulo
+ALTER TABLE tareas DROP CONSTRAINT tareas_titulo_key;
+
+
+CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
+
+ALTER TABLE usuarios ADD COLUMN gravatar VARCHAR(255)
