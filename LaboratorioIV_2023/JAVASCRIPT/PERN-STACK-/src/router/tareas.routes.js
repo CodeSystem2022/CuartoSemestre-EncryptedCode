01e1d6837cb568f8fb42869a47e60e655fd0181d
RocioPulitta <109706451+RocioPulitta@users.noreply.github.com>
2023-11-04 23:54:58 -0300

    Update tareas.routes.js

--- LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/router/tareas.routes.js
+++ LaboratorioIV_2023/JAVASCRIPT/PERN-STACK-/src/router/tareas.routes.js
@@ -1,16 +1,19 @@
 import Router from "express-promise-router";
-import { listarTareas, listarTare, crearTarea, actualizarTarea, eliminarTarea} from "../controller/tareas.controller.js";
-import { isAuth } from "../middlewares/auth.middlewares.js"
+import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controller.js"
+import {isAuth} from "../middlewares/auth.middleware.js"
+import { validateSchema } from "../middlewares/validate.middleware.js"
+import { createTareasSchema, updateTareasSchema } from "../schemas/tareas.schema.js";
+
 const router = Router();
 
-router.get('/tareas',isAuth, listarTareas),
+router.get('/tareas',isAuth, listarTareas );
 
-router.get('/tareas/:id',isAuth, listarTare);
+router.get('/tareas/:id',isAuth, listarTarea);
 
-router.post('/tareas',isAuth, crearTarea );
+router.post('/tareas',isAuth,validateSchema(createTareasSchema) ,crearTarea);
 
-router.put('/tareas/:id',isAuth, actualizarTarea);
+router.put('/tareas/:id',isAuth,validateSchema(updateTareasSchema),actualizarTarea);
 
-router.delete('/tareas/:id',isAuth, eliminarTarea)
+router.delete('/tareas/:id',isAuth, eliminarTarea);
 
 export default router;
